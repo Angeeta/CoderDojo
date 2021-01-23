3193643baf04d33cca1a0fcc6b6e27d884aa28ba
akima <akima@LAPTOP-3GIH7399.lan>
2021-01-23 11:12:19 -0800

    Started Operations

--- coderdojo/src/corejava/Operators.java
+++ coderdojo/src/corejava/Operators.java
@@ -5,9 +5,21 @@ public class Operators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("potato");
-		System.out.println("potato");
+		System.out.println("Hello");
+		System.out.println(":)");
+		
+		// 2 rabbits : Butter & Toast and their comparisons
+		
+		int butterWeight = 3;
+		int toastWeight = 4;
+		
+		System.out.println("Butter weighs " + butterWeight + " pounds");
+		System.out.println("Toast weighs " + toastWeight + " pounds");
+		
+		int toastWeighsMore = toastWeight - butterWeight;
 
+		System.out.println("Toast weighs " + toastWeighsMore + " more pound than butter");
+		
 	}
 
 }
