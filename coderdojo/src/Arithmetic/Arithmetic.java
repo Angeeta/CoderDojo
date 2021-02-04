26087a8899a6e14407b30a3bba9b7e4dd0b86fbd
akima <akima@LAPTOP-3GIH7399.lan>
2021-02-03 20:31:45 -0800

    base

--- coderdojo/src/Arithmetic/Arithmetic.java
+++ coderdojo/src/Arithmetic/Arithmetic.java
@@ -2,9 +2,32 @@ package Arithmetic;
 
 public class Arithmetic {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {}
 		// TODO Auto-generated method stub
+	
+            public int add(int number1, int number2) {
+		        int sum;
+		        sum = number1 + number2;
+		        return sum;
+		    }
 
+		    public  int subtraction(int number1, int number2) {
+		        int subtraction;
+		        subtraction = number1 - number2;
+		        return subtraction;
+		    }
+		        
+		    public int multiplication(int number1, int number2) {
+		    	int multiplication;
+		    	multiplication = number1 * number2;
+		    	return multiplication;
+		    }
+		    
+		    public int division(int number1, int number2) {
+		    	int division;
+		    	division = number1 / number2;
+		    	return division;
+		    }
+		     
 	}
 
-}
---
+++ coderdojo/src/Main/Main.java
@@ -0,0 +1,31 @@
+package Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		public class main {
+
+		    public static void main(String[] args) {
+
+		        Arithmetic math = new Arithmetic();
+
+		        int add = math.add(1, 2);
+		        System.out.println("One plus Two is " + add);
+
+		        int subtract = math.subtraction(2, 1);
+		        System.out.println("Two minus One is " + subtract);
+		        
+		        int multiply = math.multiplication(4, 3);
+		        System.out.println("Four times Three is " + multiply);
+		        
+		        int divide = math.division(12, 3);
+		        System.out.println("Twelve divided by Three is " + divide);
+
+		    }
+		}
+		
+	}
+
+}
