7bb59cac0cf59c8f53541aafffad6d478c83a607
akima <akima@LAPTOP-3GIH7399.lan>
2021-02-23 17:56:01 -0800

    commit

--- coderdojo/src/Bitwise/Bitwise.java
+++ coderdojo/src/Bitwise/Bitwise.java
@@ -4,7 +4,27 @@ public class Bitwise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		public int and(int number1, int number2) {
+			int outcome = number1 & number2;
+			return outcome;
+		}
+		
+		public int or(int number1, int number2) {
+			int outcome = number1 | number2;
+			return outcome;
+		}
+		
+		public int exclusiveOr(int number1, int number2) {
+			int outcome = number1 ^ number2;
+			return outcome;
+		}
+		
+		public int complement(int number) {
+			int outcome = ~number;
+			return outcome;
+		}
+	
 	}
 
 }
