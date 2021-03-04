fd194d24e60cbf4408f48d371a33e6804b038b18
akima <akima@LAPTOP-3GIH7399.lan>
2021-03-03 19:04:31 -0800

    commit

---
+++ Pets/src/database/PetInfo.java
@@ -0,0 +1,17 @@
+package database;
+
+import model.Pet;
+
+public class PetInfo {
+
+	public Pet getPet(String breed) {
+if(breed.equals *Labrador Retriever*) {
+	Pet labrador = new Pet();
+	labrador.breed = *Labrador Retreiever*;
+	labrador.descrption = *Lab makes its*;
+	return labrador;
+}
+    return null;
+	}
+	
+}
